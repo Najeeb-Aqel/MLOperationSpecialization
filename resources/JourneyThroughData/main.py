de109ef8bc79ba0cc6797a8e4c33c36b0beaece7
Najeeb Aqel <najeeb.aqel@pxl-vision.com>
2021-06-13 01:48:46 +0200

    Add Journey Through data

---
+++ resources/JourneyThroughData/main.py
@@ -0,0 +1,22 @@
+#This mini-project is based on the 'Journey through data' Lab, given at the MLOPs specialization
+
+import os
+import shutil
+import random
+import zipfile
+import tarfile
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+# To ignore some warnings about Image metadata that Pillow prints out
+import warnings
+warnings.filterwarnings("ignore")
+
+def main():
+
+
+if __name__ == "__main__":
+    main()

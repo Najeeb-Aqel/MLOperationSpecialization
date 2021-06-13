76e365b38fa0ef115608f48971a4d132d2b5e0bf
Najeeb Aqel <najeeb.aqel@pxl-vision.com>
2021-06-13 13:19:14 +0200

    preprocess bird images

--- resources/JourneyThroughData/main.py
+++ resources/JourneyThroughData/main.py
@@ -26,13 +26,30 @@ if __name__ == "__main__":
     if not os.path.isdir("./content"):
         download_artifacts()
 
-    cats_and_dogs_zip = '/content/kagglecatsanddogs_3367a.zip'
-    caltech_birds_tar = '/content/CUB_200_2011.tar'
+    cats_and_dogs_zip = './content/kagglecatsanddogs_3367a.zip'
+    caltech_birds_tar = './content/CUB_200_2011.tar'
 
-    base_dir = '/content/raw_data'
+    base_dir = './content/raw_data'
 
-    with zipfile.ZipFile(cats_and_dogs_zip, 'r') as my_zip:
-        my_zip.extractall(base_dir)
+    if not os.path.isdir("./content/raw_data"):
+        with zipfile.ZipFile(cats_and_dogs_zip, 'r') as my_zip:
+            my_zip.extractall(base_dir)
 
-    with tarfile.TarFile(caltech_birds_tar, 'r') as my_tar:
-        my_tar.extractall(base_dir)
+        with tarfile.TarFile(caltech_birds_tar, 'r') as my_tar:
+            my_tar.extractall(base_dir)
+
+    base_dogs_dir = os.path.join(base_dir, 'PetImages/Dog')
+    base_cats_dir = os.path.join(base_dir, 'PetImages/Cat')
+    base_birds_dir = os.path.join(base_dir, 'PetImages/Bird')
+
+    if not os.path.isdir(base_birds_dir):
+        os.mkdir(base_birds_dir)
+        raw_birds_dir = './content/raw_data/CUB_200_2011/images'
+        for subdir in os.listdir(raw_birds_dir):
+            subdir_path = os.path.join(raw_birds_dir, subdir)
+            for image in os.listdir(subdir_path):
+                shutil.move(os.path.join(subdir_path, image), os.path.join(base_birds_dir))
+
+    print(f"There are {len(os.listdir(base_birds_dir))} images of birds")
+    print(f"There are {len(os.listdir(base_dogs_dir))} images of dogs")
+    print(f"There are {len(os.listdir(base_cats_dir))} images of cats")

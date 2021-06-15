2431ed18c68109b82e3ad2d270adc2c39213ad9a
Najeeb Aqel <najeeb.aqel@pxl-vision.com>
2021-06-16 00:04:05 +0200

    Add model.py

---
+++ resources/JourneyThroughData/model.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models, optimizers
+
+def create_model():
+    model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(128, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Flatten(),
+    layers.Dense(512, activation='relu'),
+    layers.Dense(3, activation='softmax')
+    ])
+
+
+    # Compile the model
+    model.compile(
+      loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+      optimizer=optimizers.Adam(),
+      metrics=[tf.keras.metrics.SparseCategoricalAccuracy()]
+    )
+
+    return model

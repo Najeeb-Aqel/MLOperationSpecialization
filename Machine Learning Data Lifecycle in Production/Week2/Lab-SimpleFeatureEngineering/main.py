0e80abf467a9a0c8217a8e999746474b3415a276
Najeeb Aqel <najeeb.aqel@pxl-vision.com>
2021-07-18 17:04:09 +0200

    initial commit to Simple Feature Engineering lab

---
+++ Machine Learning Data Lifecycle in Production/Week2/Lab-SimpleFeatureEngineering/main.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+import tensorflow_transform as tft
+import tensorflow_transform.beam as tft_beam
+
+from tensorflow_transform.tf_metadata import dataset_metadata
+from tensorflow_transform.tf_metadata import schema_utils
+
+import pprint
+import tempfile
+
+def main():
+
+
+
+if __name__ == "__main__":
+    main()
